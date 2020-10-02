87b33043aab5e30f401a2899329834ecab831c69
Shungi <rossx511@umn.edu>
2020-10-02 16:24:36 -0500

    Initial Commit. Downloads file and saves to a file created in the working directory

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+## John Hopkins Getting and Cleaning Data Course Project
+## Kaala Malone
+## October 2020
+
+# Data obtained from UCI Machine Learning Repository
+# Human Activity Recognition Using Smartphones Data Set
+# More information about this dataset can be found at
+# http://archive.ics.uci.edu/ml/datasets/Human+Activity
+# +Recognition+Using+Smartphones#
+
+
+URL <-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles
+%2FUCI%20HAR%20Dataset.zip"
+
