3bf68b03f13f22d329589ce9af65087c7470ab15
Shungi <rossx511@umn.edu>
2020-10-07 16:41:49 -0500

    I read in the rest of the test files and started editing and combining them

--- run_analysis.R
+++ run_analysis.R
@@ -10,6 +10,7 @@
 #
 #
 ##############################
+library(tidyverse)
 
 #Download Data, Save to working directory and unzip files
 
@@ -29,3 +30,36 @@ unzip(zipfile ="./PhoneActivityData.zip", exdir = getwd())
 
 list.files()
 
+# grab needed files out of folder for training set, I set options
+# to digits = 8 because it was bugging me that it didn't print all the digits 
+# past the decimal
+
+options(digits = 8)
+
+#features contains all the variable names needed for both the test and training 
+# data
+
+features<-read_lines("./UCI HAR Dataset/features.txt")
+
+old_activity <-c("1","2","3","4","5","6")
+new_activity <-c("walking","walkingupstairs","walkingdownstairs","sitting",
+                 "standing","laying")
+
+test_values<- read_table("./UCI HAR Dataset/test/X_test.txt", 
+                         col_names = features)
+
+test_subjects <-read_lines("./UCI HAR Dataset/test/subject_test.txt")
+
+#read in the test activity
+
+test_activity <-read_lines("./UCI HAR Dataset/test/y_test.txt")
+
+#replace the numbers with descriptive 
+for(i in 1:6)
+{
+        test_activity<-sub(old_activity[i],new_activity[i],test_activity)
+}
+test_activity
+
+test_values <-test_values %>% select(contains("mean")|contains("std"))
+     
